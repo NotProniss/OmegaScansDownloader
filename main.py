3871e67a1b2d2fb07f3afaae9cafe6a3fdf378b3
NotProniss <proniss@protonmail.com>
2025-08-26 23:11:11 -0700

    Fix image download and HTTP 403 error

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import lxml
 
 imagenum = 0
 chapter_explain = '!under construction! Chapters You Wanna Scrape, Example: -C 12 [Gets Chapter 12], -C 1~4 [Gets Chapter 1, 2, 3, 4]'
-name = ""
+name = "set it"
 parsed_url = ''
 
 def check_url(in_url):
@@ -35,31 +35,51 @@ def better_dirname(dirname):
 
 def create_dir(html):
     soup = bs4(html, 'lxml')
-    # h1s = soup.find('h1', {'class': 'entry-title'}).text
-    # print(h1s)
     global name
-    div = soup.find('div', {'class': 'allc'})
-    soup2 = bs4(str(div), 'lxml')
-    for a in soup2.find_all('a', href=True):
-        name = a.text
-    name = better_dirname(name)
+    # Try to get manga name from <h1 class="entry-title"> or fallback to URL
+    h1 = soup.find('h1', {'class': 'entry-title'})
+    if h1 and h1.text.strip():
+        name = better_dirname(h1.text.strip())
+    else:
+        # fallback: use last part of URL
+        from urllib.parse import urlparse
+        path = urlparse(sys.argv[-1]).path
+        name = better_dirname(path.strip('/').split('/')[-2] if '/' in path else path)
+    if not name:
+        print("Could not determine manga name. Exiting.")
+        sys.exit(1)
     if not os.path.exists(name):
         os.makedirs(name)
 
 def get_list(html):
     global imagenum
     soup = bs4(html, 'html.parser')
-    soup
-    #print("Initiating The Download of "+ soup.find('h3', {'class': 'entry-title'}))
-    imgtags = soup.find_all('img', {'class': 'alignnone'})
-    for image in imgtags:
-        print("Downloading " + image['src'] + " 🐱‍🏍")
-        get_image(image['src'])
-        imagenum = imagenum + 1
+    # Find all <div class="flex flex-col justify-center items-center">
+    divs = soup.find_all('div', {'class': 'flex flex-col justify-center items-center'})
+    for div in divs:
+        imgtags = div.find_all('img')
+        for image in imgtags:
+            img_url = image.get('src') or image.get('data-src')
+            if img_url:
+                print("Downloading " + img_url + " 🐱‍🏍")
+                get_image(img_url)
+                imagenum += 1
 
 def get_image(url):
-    print()
-    wget.download(url, out=name)
+    import os
+    import requests
+    from urllib.parse import urlparse
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers, stream=True)
+        response.raise_for_status()
+        filename = os.path.basename(urlparse(url).path)
+        filepath = os.path.join(name, filename)
+        with open(filepath, 'wb') as f:
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
+    except Exception as e:
+        print(f"Failed to download {url}: {e}")
 
 def initiate(url):
     check_url(url)
